fc57be609eeef2eb560ac1f0079417de44b5d0f2
85255 <zyangcg@connect.ust.hk>
2024-02-18 14:57:33 +0800

    Add 2 print lines as report heading This is an initial commit for Comp3111LEx Project

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,6 +4,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Welcome to Scientific Calculator!");
+        System.out.println("Program Started...");
         System.out.println(b + " to power" + n + "=" + myLibrary.Power(b,n));
         System.out.println(n + "!=" + myLibrary.factortial((n)));
         System.out.println("Program..Ended...");
